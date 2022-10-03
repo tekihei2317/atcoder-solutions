fca393ab17eb4cdde35b6a785f5c33d55435d488
tekihei2317 <tekihei2317@gmail.com>
2022-10-03 11:02:50 +0900

    Bind function

--- practice/fsharp/practice4.fsx
+++ practice/fsharp/practice4.fsx
@@ -71,3 +71,121 @@ module ComputationExpressionDivision =
 
     printfn "good: %A" (divideByWorkflow2 12 3 2 1)
     printfn "bad: %A" (divideByWorkflow2 12 3 0 1)
+
+module ContinuationExample =
+    let divide ifZero ifSuccess a b =
+        if b = 0 then ifZero () else ifSuccess (a / b)
+
+    let divideAndPrint =
+        let ifZero () = printfn "bad"
+        let ifSuccess x = printfn "good %d" x
+        divide ifZero ifSuccess
+
+    let divideAndConvertToOption =
+        let ifZero () = None
+        let ifSuccess x = Some(x)
+        divide ifZero ifSuccess
+
+    let divideAndThrowException =
+        let ifZero () = failwith "divide by 0"
+        let ifSuccess x = x
+        divide ifZero ifSuccess
+
+    divideAndPrint 6 3
+    divideAndPrint 6 0
+    divideAndConvertToOption 6 3
+    divideAndConvertToOption 6 0
+    divideAndThrowException 6 3
+// divideAndThrowException 6 0
+
+module BindExample =
+    // これは
+    // let! x = 10
+    // printfn "%d" x
+
+    // これと同じ
+    // Bind(10, (fun x -> printfn "%d", x))
+
+    let (<<=) m f =
+        printfn "expression is %A" m
+        f m
+
+    let loggingWorkflow = 1 <<= (+) 2 <<= (*) 42 <<= id
+
+module DivideByWithBindFunction =
+    let divideBy bottom top =
+        if bottom = 0 then None else Some(top / bottom)
+
+    let bind m f = Option.bind f m
+    let return' x = Some x
+
+    let divideByWorkflow x y z w =
+        bind (x |> divideBy y) (fun a -> bind (a |> divideBy w) (fun b -> return' b))
+
+    let good = divideByWorkflow 12 3 2 1
+    printfn "good: %A" good
+
+module DivideByWithComputationExpression =
+    let divideBy bottom top =
+        if bottom = 0 then None else Some(top / bottom)
+
+    type MaybeBuilder() =
+        member this.Bind(m, f) = Option.bind f m
+        member this.Return(x) = Some(x)
+
+    let maybe = new MaybeBuilder()
+
+    let divideByWorkflow x y z w =
+        maybe {
+            let! a = x |> divideBy y
+            let! b = a |> divideBy z
+            let! c = b |> divideBy w
+            return c
+        }
+
+    let good = divideByWorkflow 12 3 2 1
+    printfn "good: %A" good
+
+module StringAddWorkflow =
+    let strToInt x =
+        try
+            Some(x |> int)
+        with :? System.FormatException ->
+            None
+
+    printfn "good: %A" ("123" |> strToInt)
+    printfn "bad: %A" ("aaa" |> strToInt)
+
+    type MaybeBuilder() =
+        member this.Bind(m, f) = Option.bind f m
+        member this.Return(x) = Some(x)
+
+    let maybe = new MaybeBuilder()
+
+    let stringAddWorkflow x y z =
+        maybe {
+            let! a = strToInt x
+            let! b = strToInt y
+            let! c = strToInt z
+            return a + b + c
+        }
+
+    let stringAddWorkflow2 x y z =
+        let a = x |> strToInt
+        let b = y |> strToInt
+        let c = z |> strToInt
+
+        match a with
+        | None -> None
+        | Some (x) ->
+            match b with
+            | None -> None
+            | Some (y) ->
+                match c with
+                | None -> None
+                | Some (z) -> Some(x + y + z)
+
+    stringAddWorkflow "12" "3" "2" |> printfn "good: %A"
+    stringAddWorkflow "12" "xyz" "2" |> printfn "bad: %A"
+    stringAddWorkflow2 "12" "3" "2" |> printfn "good: %A"
+    stringAddWorkflow2 "12" "xyz" "2" |> printfn "bad: %A"
